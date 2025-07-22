f8957c0a7a51cdc3ccb814e3daf1850556189a24
somashekhar <Somashekharnaikn4@gmail.com>
2025-07-22 08:39:53 +0530

    commit-78

--- client/src/pages/StudentLogin.jsx
+++ client/src/pages/StudentLogin.jsx
@@ -26,7 +26,7 @@ const StudentLogin = () => {
 
     try {
       const response = await axios.post(
-        `${API_BASE}/students/login`,
+        `${API_BASE}/api/students/login`,
         formData,
       );
 
