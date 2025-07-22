e71417c72205f67e22a98129ffaff0e90dcce131
somashekhar <Somashekharnaikn4@gmail.com>
2025-07-22 22:49:46 +0530

    commit-97

--- backend/models/Analytics.js
+++ backend/models/Analytics.js
@@ -1,25 +1,23 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/db');
-
-const Analytics = sequelize.define('Analytics', {
-  date: {
-    type: DataTypes.STRING(10), // YYYY-MM-DD
-    primaryKey: true,
-  },
-  hits: {
-    type: DataTypes.INTEGER,
-    defaultValue: 0,
-  },
-  uniqueVisitors: {
-    type: DataTypes.INTEGER,
-    defaultValue: 0,
-  },
-  uniqueIps: {
-    type: DataTypes.TEXT, // JSON string of IPs
-  },
-}, {
-  tableName: 'analytics',
-  timestamps: false,
-});
-
-module.exports = Analytics; 
+module.exports = (sequelize, DataTypes) => {
+  const Analytics = sequelize.define('Analytics', {
+    date: {
+      type: DataTypes.STRING(10), // YYYY-MM-DD
+      primaryKey: true,
+    },
+    hits: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    uniqueVisitors: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    uniqueIps: {
+      type: DataTypes.TEXT, // JSON string of IPs
+    },
+  }, {
+    tableName: 'analytics',
+    timestamps: false,
+  });
+  return Analytics;
+}; 
