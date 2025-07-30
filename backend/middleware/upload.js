a2c933e8898c0e2e8519ddf7833fb60d46314b89
somashekhar <Somashekharnaikn4@gmail.com>
2025-07-30 23:00:52 +0530

    commit-160

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -2,14 +2,7 @@ const multer = require('multer');
 const path = require('path');
 
 // Memory storage for BLOB upload
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/');
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname);
-  }
-});
+const storage = multer.memoryStorage();
 
 // File filter for different upload types
 const fileFilter = (req, file, cb) => {
